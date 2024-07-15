dc594629393df59f7c357e63625ba62e8a987639
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2024-07-15 16:20:13 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -9,13 +9,27 @@ def call() {
                     }
                 }
             }
-            stage ('Build') {
-                steps {
-                    script {
-                        sh 'npm install'
-                    }
-                }
-            }
+            tools {
+        maven 'maven3' 
+    }
+      stage ('Build') {
+        steps {
+          script{
+            def mvnHome = tool name: 'maven3', type: 'maven'
+            sh "${mvnHome}/bin/mvn clean package"
+              sh 'mv target/onlinebookstore*.war target/mybook.war'
+              // sh "mvn clean package"
+              // sh "mv target/*.war target/mybook.war"
+          }
+        }
+      }
+            // stage ('Build') {
+            //     steps {
+            //         script {
+            //             sh 'npm install'
+            //         }
+            //     }
+            // }
             // stage ('SonarQube Analysis') {
             //     steps {
             //         script { 
